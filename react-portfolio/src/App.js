37ec84e37ec052405fc66fc3299068474c17d4cc
Lauren Hooper <lluther921@gmail.com>
2020-11-20 18:15:30 -0800

    persistent drawer nav and header component created

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+  return (
+    <div >
+      <Navbar />
+     <Header />
+        
+        
+    
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LazyHero from 'react-lazy-hero';
+
+function Header() {
+    return (
+        <div>
+            <LazyHero imageSrc="../images/mitchell-luo-4vBN9dMCip0-unsplash.jpg">
+                <h1>Generic Startup Hype Headline</h1>
+            </LazyHero>
+
+            {/* ... */}
+        </div>
+    );
+}
+
+
+export default Header
