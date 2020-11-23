77b3b77ee5696991e43ec31fe1f267abb070b0a3
Lauren Hooper <lluther921@gmail.com>
2020-11-22 17:39:29 -0800

    change pkg json homepage path

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -25,6 +25,7 @@ class MainPage extends Component {
 					<NavBar />
 					<Switch>
 						<Route path="/" exact component={Home} />
+						<Route path="/Home" exact component={Home} />
 						<Route path="/About" component={About} />
 						<Route path="/Portfolio" component={Portfolio} />
 						<Route path="/Contact" component={Contact} />
