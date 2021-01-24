bb52b24b3b37edf2cf8db46ae3a4d88f4084c619
Lauren Hooper <lluther921@gmail.com>
2021-01-23 18:11:00 -0800

    trying hashrouter

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // Import Materialize
 import M from 'materialize-css';
